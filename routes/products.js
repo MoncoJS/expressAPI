afff71bf822fd0c2cb3bf506558e887a457367fc
MoncoJS <moncojs@gmail.com>
2025-06-18 21:05:13 +0700

    Post Products

--- routes/products.js
+++ routes/products.js
@@ -1,17 +1,36 @@
 var express = require('express');
+const productModel = require('../models/product');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.send('method get');
 });
-router.post('/', function(req, res, next) {
-  res.send('method post');
+router.post('/', async function(req, res) {
+    try {
+        const { product_name, price, amount } = req.body;
+        let newProduct = new productModel({
+            product_name: product_name,
+            price: price,
+            amount: amount
+        });
+        let savedProduct = await newProduct.save();
+        return res.status(201).send({
+            data: savedProduct,
+            message: 'Product created successfully',
+            success: true
+        });
+    } catch (error) {
+        return res.status(500).send({
+            message: 'Error creating product',
+            success: false,
+        });
+    }
 });
-router.put('/', function(req, res, next) {
+router.put('/', function(req, res) {
   res.send('method put');
 });
-router.delete('/', function(req, res, next) {
+router.delete('/', function(req, res) {
   res.send('method delete');
 });
 
